1102c42f311c158c031e7dc6b733546b53c365c1
Evan Mckenzie <615evan@gmail.com>
2022-08-11 13:13:56 -0400

    SyntaxSugarExercise

---
+++ SyntaxSyntaxSugarExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿//int answer = 4;
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+var answer = 4;
+var solution = (answer < 9)
+    ? $"{answer} is less than 9"
+    : $"{answer} is greater than 9";
+Console.WriteLine(solution);
